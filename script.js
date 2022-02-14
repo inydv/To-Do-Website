a471b9b7f71b03bf95ae85cac6877a2df1a99be8
Lokesh Yadav <i_nydv@hotmail.com>
2022-02-14 13:35:04 +0530

    Adding To-Do Typing Task

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const body = document.body
 const hiddenIcon = document.querySelectorAll('.icons')
 const btn = document.querySelector('.btn')
 const circle = document.querySelector('.circle')
+const addToDo = document.querySelector('#main-2')
+const main = document.querySelector('#main')
 
 let darkMode = true
 
@@ -35,3 +37,11 @@ function changeModeToDark(){
     body.style.backgroundColor = '#1f1d1c'
     // btn.classList.toggle('darkmode')
 }
+
+addToDo.addEventListener('click', () => {
+    const enterText = document.createElement('div')
+    enterText.classList.add('content')
+    enterText.innerHTML = `<input class="write-content" type="text" placeholder="Type Here...">
+    <i class="fa-solid fa-circle-notch circle-2"></i>`
+    main.appendChild(enterText)
+})
