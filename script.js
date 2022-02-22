a428292b1ead6c00fac484be6497306fc8e7f76c
Lokesh Yadav <i_nydv@hotmail.com>
2022-02-22 15:21:22 +0530

    Adding Strike

--- script.js
+++ script.js
@@ -1,109 +1,102 @@
-const body = document.body
-const hiddenIcon = document.querySelectorAll('.icons')
+const body = document.body;
+const hiddenIcon = document.querySelectorAll(".icons");
 
-const input = document.getElementById('input')
-const btn = document.getElementById('btn')
-const listContainer = document.getElementById('list-container')
-
-const context = document.querySelectorAll('.context')
+const input = document.getElementById("input");
+const btn = document.getElementById("btn");
+const listContainer = document.getElementById("list-container");
 
 // DARK MODE & Light Mode
 
-let darkMode = true
+let darkMode = true;
 
-hiddenIcon.forEach(icons => {
-    icons.addEventListener('click',() => {
-        removeHiddenIcon()
-        icons.classList.add('hidden-icon')
-    })
-})
+hiddenIcon.forEach((icons) => {
+  icons.addEventListener("click", () => {
+    removeHiddenIcon();
+    icons.classList.add("hidden-icon");
+  });
+});
 
-function removeHiddenIcon(){
-    hiddenIcon.forEach(icons => {
-        icons.classList.remove('hidden-icon')
-    })
+function removeHiddenIcon() {
+  hiddenIcon.forEach((icons) => {
+    icons.classList.remove("hidden-icon");
+  });
 
-    if(darkMode === true) {
-        changeModeToLight()
-        darkMode = false
-    } else if(darkMode === false) {
-        changeModeToDark()
-        darkMode = true
-    }
+  if (darkMode === true) {
+    changeModeToLight();
+    darkMode = false;
+  } else if (darkMode === false) {
+    changeModeToDark();
+    darkMode = true;
+  }
 }
 
-function changeModeToLight(){
-    body.style.backgroundColor = 'White'
+function changeModeToLight() {
+  body.style.backgroundColor = "White";
 }
 
-function changeModeToDark(){
-    body.style.backgroundColor = '#1f1d1c'
+function changeModeToDark() {
+  body.style.backgroundColor = "#1f1d1c";
 }
 
 // ADDING LIST
 
-showTask()
+showTask();
 
-btn.addEventListener('click', () => {
-    let text = input.value;
+btn.addEventListener("click", () => {
+  let text = input.value;
 
-    if (text.trim() != 0) {
-
-        let LocalStorage = localStorage.getItem("arr")
-        if(LocalStorage == null) {
-            obj = []
-        }
-        else{
-            obj = JSON.parse(LocalStorage)
-        }
-        obj.push(text)
-        localStorage.setItem("arr", JSON.stringify(obj))
-
-        input.value = ''
-        showTask()
+  if (text.trim() != 0) {
+    let LocalStorage = localStorage.getItem("arr");
+    if (LocalStorage == null) {
+      obj = [];
+    } else {
+      obj = JSON.parse(LocalStorage);
     }
-    else {
-        alert("Enter Text")
-    }
-})
+    obj.push(text);
+    localStorage.setItem("arr", JSON.stringify(obj));
 
-function showTask(){
+    input.value = "";
+    showTask();
+  } else {
+    alert("Enter Text");
+  }
+});
 
-    let LocalStorage = localStorage.getItem("arr")
-    if(LocalStorage == null) {
-        obj = []
-    }
-    else{
-        obj = JSON.parse(LocalStorage)
-    }
+function showTask() {
+  let LocalStorage = localStorage.getItem("arr");
+  if (LocalStorage == null) {
+    obj = [];
+  } else {
+    obj = JSON.parse(LocalStorage);
+  }
 
-    listContainer.innerHTML = ''
+  listContainer.innerHTML = "";
 
-    obj.forEach((item, index) => {
-        let list = document.createElement('li');
-        list.classList.add('content')
-        list.innerHTML = 
-        `<p class="context">${item}</p>
+  obj.forEach((item, index) => {
+    let list = document.createElement("li");
+    list.classList.add("content");
+    list.innerHTML = `<p class="context">${item}</p>
         <i class="fa-regular fa-trash-can bin" onclick="deleteItem(${index})"></i>
-        <i class="fa-solid fa-strikethrough striking" onclick="strikeItem(${index})"></i>`
-        listContainer.insertBefore(list,listContainer.childNodes[0])
-    })
+        <i class="fa-solid fa-strikethrough striking" onclick="strikeItem(${index})"></i>`;
+    listContainer.insertBefore(list, listContainer.childNodes[0]);
+  });
 }
 
 // ADDING STRIKE
 
-function strikeItem(index) {
-    console.log(index)
-    context[index].classList.toggle('unactive')
+function strikeItem(index){
+    const context = document.querySelectorAll(".context");
+    let idx = context.length - ( index + 1 )
+    context[idx].classList.toggle("unactive")
 }
 
 // Adding bin
 
 function deleteItem(index) {
-    let LocalStorage = localStorage.getItem("arr")
-    obj = JSON.parse(LocalStorage)
+  let LocalStorage = localStorage.getItem("arr");
+  obj = JSON.parse(LocalStorage);
 
-    obj.splice(index, 1)
-    localStorage.setItem("arr", JSON.stringify(obj))
-    showTask()
+  obj.splice(index, 1);
+  localStorage.setItem("arr", JSON.stringify(obj));
+  showTask();
 }
